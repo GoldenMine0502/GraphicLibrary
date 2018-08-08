62c405ee299b4051f2b98204812f253472435366
GoldenMine0502 <ehe123@naver.com>
2018-08-08 20:20:46 +0900

    stable version v1

---
+++ src/com/GoldenMine/event/EffectFader.java
@@ -0,0 +1,14 @@
+package com.GoldenMine.event;
+
+import com.GoldenMine.graphic.Palette;
+import com.GoldenMine.graphic.elements.Sprite;
+
+/**
+ * Created by ehe12 on 2018-08-08.
+ */
+public class EffectFader implements IEffect {
+    @Override
+    public void onInvoked(Palette palette, Sprite sprite, double percent, Object... parameters) {
+
+    }
+}
